6e01f093ab6e954240d209eab3e5351e47b50443
George Hoffmann <143972351+gahwings1@users.noreply.github.com>
2025-04-04 22:08:07 -0400

    Navbar Work

--- BSBfrontend/src/App.jsx
+++ BSBfrontend/src/App.jsx
@@ -10,7 +10,7 @@ function App() {
   return (
     <>{!isLoaded && <LoadingScreen onComplete={() => setIsLoaded(true)} />}{""} 
       <div className={`min-h-screen transition-opacity duration-700 ${isLoaded ? "opacity-100" : "opacity-0"} bg-black text-gray-100`}>
-
+        <Navbar />
       </div>
     </>
   );
