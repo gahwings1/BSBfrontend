f207c7fb2d2c20cc60e3781442f71ce7e8ca5c8e
George Hoffmann <143972351+gahwings1@users.noreply.github.com>
2025-04-04 22:00:09 -0400

    Front End Work

--- BSBfrontend/src/App.jsx
+++ BSBfrontend/src/App.jsx
@@ -1,13 +1,18 @@
 import { useState } from "react";
-import "./index.css";
+import "./App.css";
 import { LoadingScreen } from './components/LoadingScreen';
-import "./App.css"
+import { Navbar } from "./components/Navbar";
+import "./index.css";
 
 function App() {
-  const [isLoaded, setIsLoaded] = setState(false);
+  const [isLoaded, setIsLoaded] = useState(false);
 
   return (
-    <>{!isLoaded && <LoadingScreen onComplete={() => setIsLoaded(true)} />}</>
+    <>{!isLoaded && <LoadingScreen onComplete={() => setIsLoaded(true)} />}{""} 
+      <div className={`min-h-screen transition-opacity duration-700 ${isLoaded ? "opacity-100" : "opacity-0"} bg-black text-gray-100`}>
+
+      </div>
+    </>
   );
 }
 
